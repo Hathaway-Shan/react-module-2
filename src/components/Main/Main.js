ceb429b9ed4868504eb5da0463021f7ade7b4551
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-12 16:54:19 -0700

    header footer and background image are displaying

---
+++ src/components/Main/Main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Main.css';
+// import { animals } from '../../data.js';
+// import Animal from '../Animal/Animal.js';
+import background from '../../background.png';
+export default function Main() {
+  return <main style={{ backgroundImage: `url(${background})` }} className="App"></main>;
+}
