c9a1dc5b7a98e43e88e8301230ea4314091fbe46
Hathaway-Shan <hathaway.shan@gmail.com>
2022-09-12 17:16:21 -0700

    images and text are now displaying for all signs in main

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 import './Main.css';
 import { zodiac } from '../../data.js';
-// import Animal from '../Animal/Animal.js';
+
+import Zodiac from '../Zodiac/Zodiac.js';
 import background from '../../background.png';
 export default function Main() {
-  return <main style={{ backgroundImage: `url(${background})` }} className="App"></main>;
+  return (
+    <main style={{ backgroundImage: `url(${background})` }} className="App">
+      {zodiac.map((sign) => (
+        <Zodiac key={sign.name} {...sign} />
+      ))}
+    </main>
+  );
 }
